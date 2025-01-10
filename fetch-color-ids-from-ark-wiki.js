2a550622cfa30265e2710d582b36ba633a0e0e34
pleinx <hesse.fabian@gmail.com>
2025-01-10 18:34:35 +0100

    fix(fetchScript): missed full name of color ID

--- fetch-color-ids-from-ark-wiki.js
+++ fetch-color-ids-from-ark-wiki.js
@@ -1,7 +1,8 @@
 // Visit: https://ark.wiki.gg/wiki/Color_IDs
 // Put the code below into your code-inspector/dev tools
-Array.from(document.querySelectorAll('.color-ids-list')).forEach(list => {
-    Array.from(list.querySelectorAll('li')).forEach(color => {
-       console.log(color.querySelector('.color-id-name').innerText.replace('ID: ', '').split(' ')[0], '|', color.querySelector('.color-id-name').innerText.replace('ID: ', '').split(' ')[1], '|',color.querySelector('.color-id-infos > code').innerText)
+Array.from(document.querySelectorAll('.color-ids-list')).forEach((list) => {
+    Array.from(list.querySelectorAll('li')).forEach((color) => {
+	const colorId = color.querySelector('.color-id-name').innerText.replace('ID: ', '').split(' ')[0];
+	console.log(colorId, '|', color.querySelector('.color-id-name').innerText.replace(colorId, '').replace('ID: ', '').replace(' ', ''), '|',color.querySelector('.color-id-infos > code').innerText)
     });
 })
